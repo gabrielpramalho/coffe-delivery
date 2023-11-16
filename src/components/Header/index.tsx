bc50ab32da2517258c2ee72cf80bce5dede828f2
GABRIEL PEDRO RAMALHO <biel-pedro@hotmail.com>
2023-11-16 20:18:40 -0300

    style Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,30 @@
-import { ShoppingCart } from '@phosphor-icons/react'
+import { MapPin, ShoppingCart } from '@phosphor-icons/react'
 import logo from '../../assets/Logo.svg'
-import { HeaderContainer } from './styles'
+import { HeaderButtonContainer, HeaderContainer } from './styles'
 
 export function Header(){
+
+    const quatityItemsCart = 0
+
+    const hasItemCart = quatityItemsCart > 0
+
     return(
         <HeaderContainer>
             <a href="/">
                 <img src={logo} alt="" />
             </a>
 
-            <div>
-                <div>
+            <HeaderButtonContainer>
+                <div className='city'>
+                    <MapPin size={22} weight='fill' />
                     <span>Bandeirantes, PR</span>
                 </div>
 
-                <div>
+                <div className='cart'>
+                    {hasItemCart && <span>{quatityItemsCart}</span> }
                     <ShoppingCart size={22} weight='fill' />
                 </div>
-            </div>
+            </HeaderButtonContainer>
         </HeaderContainer>
     )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -6,4 +6,66 @@ export const HeaderContainer = styled.header`
     align-items: center;
 
     padding: 2rem .5rem;
+
+`
+
+export const HeaderButtonContainer = styled.div`
+    display: flex;
+    align-items: center;
+
+    gap: .75rem;
+
+    .city{
+        background-color: ${props => props.theme["purple-300"]};
+        color: ${props => props.theme["purple-500"]};
+
+        border-radius: 6px;
+        cursor: pointer;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 0.5rem;
+
+        span{
+            line-height: 1.3;
+            font-size: 0.875rem;
+        }
+    }
+
+    .cart{
+        position: relative;
+        background-color: ${props => props.theme["yellow-300"]};
+        color: ${props => props.theme["yellow-500"]};
+
+        padding: 0.5rem;
+
+        border-radius: 6px;
+
+        cursor: pointer;
+
+        span{
+            width: 1.25rem;
+            height: 1.25rem;
+
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            background-color: ${props => props.theme["yellow-500"]};
+            color: ${props => props.theme.white};
+
+            border-radius: 99px;
+
+            position: absolute;
+            top: -.5rem;
+            right: -.5rem;
+
+            font-size: 0.75rem;
+            font-weight: bold;
+
+            line-height: 1.3;
+            letter-spacing: -0.72px;
+        }
+    }
 `
